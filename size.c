18e43c802ef4666c69b8ba9e9ee8fc4f28325fc0
KRISHA RAVAL <71510080+2000krisha@users.noreply.github.com>
2020-09-18 07:36:40 -0700

    created size.c

---
+++ size.c
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"\n Size of int:" << sizeof(int) << "bytes";
+  cout<<"\n Size of float:" << sizeof(float) << "bytes";
+  cout<<"\n Size of double:" << sizeof(double) << "bytes";
+  cout<<"\n Size of char:" << sizeof(char) << "bytes";
+  return 0;
+}
